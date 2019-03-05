adecde5283baa1d5e2d13a5af54c132314d8fc58
pavelp0798 <pavelp0798@gmail.com>
2019-03-05 12:16:24 +0000

    Minor changes

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,19 @@ const path = require('path');
 const express = require('express');
 
 const router = express.Router();
+var participants = ['Participant 1', 'Participant 2', 'Participant 3'];
 
 router.get('/', (req, res, next) => {
   var participant = req.query.p || "p1";
   var a = req.query.a;
   var removeZeros = req.query.z;
-
-  res.render('index', { participant: participant, title: "First Experiment", removeZeros: removeZeros, types: ['calories', 'heartrate', 'steps', 'distance']});
+  res.render('index', { participants: participants, participant: participant, title: "First Experiment", removeZeros: removeZeros, types: ['calories', 'heartrate', 'steps', 'distance']});
 });
+
 router.get('/second', (req, res, next) => {
   var participant = req.query.p || "p1";
   var removeZeros = req.query.z;
-
-  res.render('second', { participant: participant, title: "Second Experiment", removeZeros: removeZeros, types: ['calories', 'calories2', 'heartrate', 'steps', 'steps2']});
-
+  res.render('second', { participants: participants, participant: participant, title: "Second Experiment", removeZeros: removeZeros, types: ['calories', 'calories2', 'heartrate', 'steps', 'steps2']});
 });
 
 module.exports = router;
